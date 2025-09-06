67ac9070e45864e94c197c00cc3857b99522e6fb
malcom-mo <m@mal.com.de>
2025-09-06 22:48:19 +0200

    Initial skinner formula

---
+++ Formula/skinner.rb
@@ -0,0 +1,42 @@
+class Skinner < Formula
+  desc "Terminal theme manager with macOS dark mode sync"
+  homepage "https://github.com/malcom-mo/skinner"
+  url "https://github.com/malcom-mo/skinner/archive/refs/tags/v1.0.0.tar.gz"
+  license "MIT"
+  version "1.0.0"
+
+  depends_on :macos
+  depends_on "rust" => :build
+  depends_on "swift" => :build
+
+  def install
+    system "cargo", "build", "--release", "--bin", "skinner", "--verbose"
+    bin.install "target/release/skinner"
+
+    system "swiftc", "skinner-sync.swift", "-o", "skinner-sync"
+    bin.install "skinner-sync"
+  end
+
+  service do
+    run [bin/"skinner-sync"]
+    keep_alive true
+    log_path var/"log/skinner-sync.log"
+    error_log_path var/"log/skinner-sync-error.log"
+  end
+
+  def caveats
+    <<~EOS
+      Skinner has been installed successfully!
+      
+      To complete the setup, start the background process and set up your shell(s):
+
+        brew services start skinner
+        source <(skinner setup)        # zsh/bash
+        skinner setup --fish | source  # fish
+    EOS
+  end
+
+  test do
+    system "#{bin}/skinner", "--help"
+  end
+end
---
+++ README.md
@@ -0,0 +1,10 @@
+# Homebrew Tap
+
+Formulas:
+- [Skinner](https://github.com/malcom-mo/skinner)
+
+## How to install
+
+`brew install malcom-mo/tap/<formula>`
+
+Or `brew tap malcom-mo/tap` and then `brew install <formula>`.
